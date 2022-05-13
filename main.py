43b2882b18dd070072444c5072e3e5af1cf2aad2
Pokrzw <paulinab2014@interia.pl>
2022-05-13 21:38:53 +0200

    Added stuff to readme

--- main.py
+++ main.py
@@ -25,13 +25,24 @@ async def add_task(ctx, task: str, date: str):
     # date = datetime.datetime(int(date_data[2]),int(date_data[1]),int(date_data[0]))
     await ctx.respond("Task created!")
 
-@bot.slash_command(name="check_task", description="Command for sending proof of completion. Use this command right after sending the proof")
+@bot.slash_command(name="check_task", description="Command for sending proof of completion. Use this command replying to the proof")
+#check task -> proof to post, na ktory odpowiadasz, a nie poprzednia wiadomosc, reakcja nie moze byc od osoby,
+#ktora jest autorem oryginalnej wiadomosci
 async def check_task(ctx):
     thread = ctx.interaction.channel
-    proof = thread.last_message
-    if(len(proof.reactions) > 0):
-        await thread.delete()
-    else:
-        await ctx.respond("Task not fulfilled")
+    proof = await ctx.interaction.original_message()
+    print(proof)
+    # if(len(proof.reactions) > 0):
+    #     await thread.delete()
+    # else:
+    await ctx.respond("Task not fulfilled")
 
+#submit_for_revision -> daje powiadomienia na kanale
+@bot.slash_command(name="submit_for_revision", description="Use when you submit a proof o completion")
+async def submit_for_revision(ctx):
+    #1. Wysyła powiadomienie do wszystkich
+    #2 [OP] has posted a proof of completing a task. Give them a reaction if they completed it!
+    original_poster = ctx.interaction.user
+    print(dir(original_poster))
+    await ctx.respond(f"@everyone  {original_poster.name} has just posted a proof of completion in [odnosnik do watku]. Give them a reaction if they completed it! ")
 bot.run(env['BOT_TOKEN'])
