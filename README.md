c67fe608b61a41765795088fb31f57fc55888e21
Pokrzw <paulinab2014@interia.pl>
2022-05-13 23:41:27 +0200

    ...

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # discord-study-bot
 Discord bot for my study channel
+
+### Tasks
+- [x] add a task feature
+- [ ] shoutout to other people 
+- [ ] ending task
+- [ ] checking if task expired
+- [ ] punishing for overdue tasks
--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import datetime
 env = dotenv_values(".env")
 bot = discord.Bot(debug_guilds=[973656184076767322])
 
-
+listened_threads = []
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
-
 
 @bot.slash_command(name="add_task", description="Command for adding tasks. Add them in format: [task] [date]. Date in format day.month.year")
 @option("task", types.string, description="Enter the task you want to do")
@@ -19,30 +18,21 @@ async def add_task(ctx, task: str, date: str):
     channel = ctx.interaction.channel
     message = await channel.send(f"{task} {date}")
     thread = await channel.create_thread(name=task, message=message)
+    original_poster = ctx.interaction.message
+    print(original_poster)
     await thread.add_user(ctx.interaction.user)
     await thread.remove_user(bot.user)
-    # date_data = date.split('.')
-    # date = datetime.datetime(int(date_data[2]),int(date_data[1]),int(date_data[0]))
+    date_data = date.split('.')
+    print(date_data)
+    date = datetime.datetime(int(date_data[2]),int(date_data[1])-1,int(date_data[0]))
     await ctx.respond("Task created!")
 
-@bot.slash_command(name="check_task", description="Command for sending proof of completion. Use this command replying to the proof")
-#check task -> proof to post, na ktory odpowiadasz, a nie poprzednia wiadomosc, reakcja nie moze byc od osoby,
-#ktora jest autorem oryginalnej wiadomosci
-async def check_task(ctx):
-    thread = ctx.interaction.channel
-    proof = await ctx.interaction.original_message()
-    print(proof)
-    # if(len(proof.reactions) > 0):
-    #     await thread.delete()
-    # else:
-    await ctx.respond("Task not fulfilled")
 
 #submit_for_revision -> daje powiadomienia na kanale
 @bot.slash_command(name="submit_for_revision", description="Use when you submit a proof o completion")
 async def submit_for_revision(ctx):
-    #1. Wysyła powiadomienie do wszystkich
-    #2 [OP] has posted a proof of completing a task. Give them a reaction if they completed it!
+    channel = ctx.interaction.channel.channel_id
+    listened_threads.push(channel)
     original_poster = ctx.interaction.user
-    print(dir(original_poster))
-    await ctx.respond(f"@everyone  {original_poster.name} has just posted a proof of completion in [odnosnik do watku]. Give them a reaction if they completed it! ")
+    await ctx.respond(f"@everyone  {original_poster.name} has just posted a proof of completion. Give them a reaction if they completed it! ")
 bot.run(env['BOT_TOKEN'])
